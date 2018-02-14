208e08dfd5390e636273a3da7959a9bd6086cb50
Navon Francis <navonfrancis@gmail.com>
2018-02-13 22:02:08 -0500

    test

---
+++ a1/assignment1.py
@@ -0,0 +1,8 @@
+# Homework 1
+# Navon Francis
+# Alexander Manztaris
+# Statistics III
+
+W = [2.05, 1.8, 2.2, 2.15, 1.75, 1.95]
+population_mean = sum(W) / len(W)
+print('population mean =', round(population_mean, 3))
